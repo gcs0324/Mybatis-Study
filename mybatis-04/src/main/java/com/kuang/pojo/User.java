60f03712fa5e20871a60d2ceb7af4448e89ec28f
cs0324 <1125720545@qq.com>
2021-03-13 21:27:48 +0800

    分页&&lombok

--- mybatis-04/src/main/java/com/kuang/pojo/User.java
+++ mybatis-04/src/main/java/com/kuang/pojo/User.java
@@ -1,46 +1,17 @@
 package com.kuang.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//无参构造，get,set,tostring,hashcode,equals
+@Data
+//有参构造
+@AllArgsConstructor
+//无参构造
+@NoArgsConstructor
 public class User {
     private int id;
     private String name;
     private String password;
-
-    public User(int id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
